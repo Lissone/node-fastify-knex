382255574d9c28677a698870d8d7d661bb4e61c3
Lissone <leonardo.lissonez@gmail.com>
2024-04-26 18:42:56 -0300

    feat: categorize tests

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,6 @@
-import { afterAll, beforeAll, test } from 'vitest';
-import request from 'supertest';
-import { app } from '../src/app';
+import { app } from './app';
+import { env } from './env';
 
-beforeAll(async () => {
-  await app.ready();
-});
-
-afterAll(async () => {
-  await app.close();
-});
-
-test('user can create a new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201);
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP Server is running on port: ${env.PORT}`);
 });
